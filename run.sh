877b0f2a13617ec4f0c703c27556c98a905d8c34
jiangmj <jiangmj@aeroht.com>
2026-01-28 17:58:07 +0800

    添加工具库

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+# set token
+#cat > "$HOME/.pypirc" << EOF
+#...
+#EOF
+
+# build
+rm -rf dist src/*.egg-info
+python3 -m pip install --upgrade build
+python3 -m build
+
+# upload
+python3 -m pip install --upgrade twine
+#python3 -m twine upload --repository pypi dist/* --verbose
+python3 -m twine upload --repository testpypi dist/* --verbose
+
+# Usage
+# pip install -U -i https://test.pypi.org/simple/ pylib-test
+# pip install -U pylib
